8b8085e30a5ec7ddc433c1865dab5444d12e580d
Gabriel Vitor <98486966+NewGabrielVi@users.noreply.github.com>
2024-10-13 01:01:34 -0300

    refactor: alter settings.py

--- eject/settings.py
+++ eject/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ejectapp.apps.EjectAppConfig',
+    'eject_app.apps.EjectAppConfig',
 ]
 
 MIDDLEWARE = [

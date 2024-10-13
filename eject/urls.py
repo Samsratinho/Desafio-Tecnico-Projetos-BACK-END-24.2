fdb0e3e7f23223ae349a53475918d895160d3064
Gabriel Vitor <98486966+NewGabrielVi@users.noreply.github.com>
2024-10-13 03:47:02 -0300

    feature

--- eject/urls.py
+++ eject/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from eject_app.views import home
+from eject_app.views import ahome
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", home),
+    path("", ahome),
 ]

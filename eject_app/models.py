49a1ae251f7dd3cb57c11f29cca71ba4443ea034
Gabriel Vitor <98486966+NewGabrielVi@users.noreply.github.com>
2024-10-13 02:29:17 -0300

    feature:create hotfix

--- eject_app/models.py
+++ eject_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class home(models.Model):
+        title = models.CharField(max_length=100,null=False, blank=False );
+        descricao = models.TextField();
+        imagem = models.ImageField(upload_to='')
+        
+        

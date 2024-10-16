5d2d795dfde819e08d5f88040902c6f7f9e68eb3
Arthur Emanuel Souza Cassiano da Costa <arthurcosta0ac@gmail.com>
2024-10-16 13:34:04 -0300

    feat: adicionando comentários via formulário

--- forum/urls.py
+++ forum/urls.py
@@ -6,4 +6,5 @@ from . import views
 app_name = 'forum'
 urlpatterns = [
     path('', views.mostrar_comentarios, name='forum'),
+    path('post/', views.salvar_comentario, name='salvar_comentario'),
 ]
--- forum/views.py
+++ forum/views.py
@@ -1,4 +1,6 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 from .models import Comentario
 
@@ -6,3 +8,18 @@ from .models import Comentario
 def mostrar_comentarios(request):
     comentarios = Comentario.objects.all().order_by('-data_criacao')
     return render(request, 'forum/forum.html', {'comentarios': comentarios})
+
+
+def salvar_comentario(request):
+    if request.method == 'POST':
+        titulo = request.POST['titulo']
+        conteudo = request.POST['caixadetexto']
+        novo_comentario = Comentario(
+            titulo=titulo,
+            autor='Cavalo anônimo',
+            conteudo=conteudo,
+        )
+        novo_comentario.save()
+        return HttpResponseRedirect(reverse('forum:forum'))
+
+    return HttpResponseRedirect(reverse('forum:forum'))

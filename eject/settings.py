eab0367f8586caab42c33e258975d89b5fb05269
Arthur Costa <88168052+arthurcostaa@users.noreply.github.com>
2024-10-15 21:21:22 -0300

    merge da branch feat/pais_e_profs com a branch dev (#1)
    * feat: tornando a página país e profs dinâmica
    
    * fix: adicionando biblioteca pillow no arquivo de requirements
    
    * refactor: removendo códigos comentados do template pais_e_profs.html
    
    * refactor: adaptando view para utilizar classe e generic view
    
    * refactor: movendo arquivos estáticos para a pasta do app pais_e_profs

--- eject/settings.py
+++ eject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pais_e_profs.apps.PaisEProfsConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'eject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,9 +117,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
---
+++ pais_e_profs/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from .models import Categoria, Material
+
+
+admin.site.register(Categoria)
+admin.site.register(Material)
